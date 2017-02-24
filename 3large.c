f5461e8472985bbdbba09dbdffd950536c89fb61
harigopi123 <hkrishanan254@gmail.com>
2017-02-24 05:23:19 -0800

    Update 3large.c

--- 3large.c
+++ 3large.c
@@ -1,19 +1,23 @@
+
 #include <stdio.h>
-int main()
-{
-    double n1, n2, n3;
 
-    printf("Enter three numbers: ");
-    scanf("%lf %lf %lf", &n1, &n2, &n3);
-
-    if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
-
-    if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
-
-    if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
-
+int main(){
+    
+    int a, b, c;
+    printf("Enter a,b,c: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if (a > b && a > c) {
+        printf("a is Greater than b and c");
+    }
+    else if (b > a && b > c) {
+        printf("b is Greater than a and c");
+    }
+    else if (c > a && c > b) {
+        printf("c is Greater than a and b");
+    }
+    else {
+        printf("all are equal or any two values are equal");
+    } 
     return 0;
 }
